8c4ed1be2944b07e8ce06d280d6e2cedc90fa7b4
Vivek Nathani <35530371+vivaan998@users.noreply.github.com>
2019-02-15 18:50:51 +0530

    Update matcher.php

--- algorithms/matcher.php
+++ algorithms/matcher.php
@@ -1,25 +1,37 @@
 <?php
 
-$keywords="java|python|c++|c";
-$words =explode('|', $keywords, -1);
-		
-	$myfile = fopen("myfile1.txt", "r") or die("Unable to open file!");
-	// Output one line until end-of-file
-	while(!feof($myfile)) {
-	$text = fgets($myfile);
+    $db2 = new mysqli('localhost', 'root', '', 'education');
+    $query = $db2->query("SELECT * FROM faculty1 WHERE Id=2");    
+    $result=mysqli_fetch_assoc($query);
+    if($result){
+        $keys=$result['keywords'];
+    }    
+//echo $keys;
+?>
+<?php
+$db3 = new mysqli('localhost', 'root', '', 'education');
+    $sql = $db3->query("SELECT * FROM student WHERE Id=21");    
+    $result1=mysqli_fetch_assoc($sql);
+    if($result1){
+        $File=$result1['File'];
+    }
+//  echo $File;
+?>
+<?php
 
-$all_keywords = $all_keys = array();
-foreach ($words as $word => $key) {
-    if (strpos(strtolower($text), strtolower($word)) !== false) {
+$words =explode('|', $keys, -1);
+$i = strlen($File);
+$all_keywords = $all_keys = $words;
+foreach ($words as $word => $key)
+{
+    if (strpos(strtolower($File), strtolower($word)) !== false) {
         $all_keywords[] = $word;
         $all_keys[] = $key;
     }
 }
-        echo $keywords_list = implode(',', $all_keywords) ."<br>";
-        echo $keys_list = implode(',', $all_keys) . "<br>";
-}
-	fclose($myfile);	
-	
+        $keywords_list = implode(',', $all_keywords);
+        $j= strlen($keywords_list);
+        $ass = (($j/$i)*100);
+        echo "Accuracy :" .$ass;
+
 ?>
-
-
